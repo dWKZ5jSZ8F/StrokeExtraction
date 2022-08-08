5adec858f3c094980ddb99c79bc3184a23f9063e
aoyi <aoyi2000@gmail.com>
2022-08-08 16:15:40 +0800

    Added bwperim1.m
    Added bwperim1.m in utils, as a modified version of contour extraction.

--- main.m
+++ main.m
@@ -1,9 +1,10 @@
 clc; clear;
-addpath('./utils/')
+addpath("./image/")
+addpath("./utils/")
 
 %% Preprocessing
 path="image/";
-filename="";
+filename="2.bmp";
 preprocess;
 
 %% Ambiguity detection
@@ -12,4 +13,9 @@ boundary=bwperim(image1,4);	% extract contour using erosion
 % Feature point detection
 skel=kmm(image);		% thinning using KMM algorithm
 
-
+%% Demo
+figure; 
+subplot(1, 2, 1)
+imshow(skel)
+subplot(1, 2, 2)
+imshow(boundary)
